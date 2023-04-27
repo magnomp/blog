558033278a6a9cf956376d55210a10b3621eced8
Magno Machado <magnomp@gmail.com>
2023-04-27 05:44:49 -0300

    Correçao na resolução do artigo

--- components/ArticleControl.vue
+++ components/ArticleControl.vue
@@ -1,18 +1,20 @@
 <script setup lang="ts">
 import { ParsedContent } from '@nuxt/content/dist/runtime/types';
 
+const route = useRoute()
+
 const props = defineProps<{
     language: string
 }>()
 
-const { data: article } = await useAsyncData('article', () => queryContent().findOne())
+const { data: article } = await useAsyncData('article', () => queryContent(route.path).findOne())
 
 </script>
 
 <template>
     <article v-if="article" class="flex flex-col shadow my-4">
-<!-- Article Image -->
-<NuxtLink :href="article._path" class="hover:opacity-75">
+        <!-- Article Image -->
+        <NuxtLink :href="article._path" class="hover:opacity-75">
             <img src="https://source.unsplash.com/collection/1346951/1000x500?sig=1">
         </NuxtLink>
         <div class="bg-white flex flex-col justify-start p-6">
@@ -20,9 +22,10 @@ const { data: article } = await useAsyncData('article', () => queryContent().fin
                 class="text-blue-700 text-sm font-bold uppercase pb-4">{{ article.tags[0] }}</NuxtLink>
             <NuxtLink :href="article._path" class="text-3xl font-bold hover:text-gray-700 pb-4">{{ article.title }}
             </NuxtLink>
-            <ArticleAuthorship :language="props.language" author="Magno Machado" :published-date="article['timestamp']"/>
+            <ArticleAuthorship :language="props.language" author="Magno Machado" :published-date="article['timestamp']" />
             <NuxtLink :href="article._path" class="pb-6">
-                <ContentRenderer :value="article"/>
+                <ContentRenderer :value="article" />
             </NuxtLink>
-    </div>
-</article></template>
+        </div>
+    </article>
+</template>
